bb465b127b543ee4ff9f6854c2c85094e0c17f17
Oleg Shnyrev <o-shnyrov@mail.ru>
2023-01-21 15:37:45 +0300

    Добавлены решения задач

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10000 && number < 100000)
+{
+    if((number % 10 == number / 10000) && ((number - (number / 10000 * 10000)) / 1000) == ((number - number % 10) % 100 / 10))
+    Console.WriteLine("да");
+    else Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("Число должно быть пятизначным.");
+}
---
+++ Task21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.WriteLine("Введите: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+// double Distance(int xa, int ya, int za, int xb, int yb, int zb);
+// double distance = Distance(x1, y1, z1, x2, y2, z2);
+int catX = x1 - x2;
+int catY = y1 - y2;
+int catZ = z1 - z2;
+double result = Math.Sqrt(catX * catX + catY * catY + catZ * catZ);
+// return result;
+
+double RResult = Math.Round(result, 2, MidpointRounding.ToZero);
+Console.WriteLine($"A ({x1}, {y1}, {z1},), B ({x2}, {y2}, {z2}), -> {RResult}");
